09ef43fa8f67304abb3f74cd5d83c50cf1b7a14d
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-03-28 18:47:55 +0530

    search functionality added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,29 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { GoogleLogin, GoogleLogout } from 'react-google-login'
 const Login = () => {
+  const statesList = [
+    'Maharashtra',
+    'Manipur',
+    'Meghalaya',
+    'Kolkata',
+    'Kochin',
+    'Kerala',
+    'Mumbai',
+    'Delhi',
+    'Dehradun',
+  ]
   const [showLogin, setShowLogin] = useState(true)
   const [showLogout, setShowLogout] = useState(false)
   const [loginDetails, setLoginDetails] = useState({})
   const [age, setAge] = useState(0)
   const [gender, setGender] = useState('')
   const [state, setState] = useState('')
+  const [formErrors, setFormErrors] = useState({
+    ageErrorState: false,
+    ageError: '',
+  })
   const clientId =
     '897828852205-isjv1j0a9gh1otrid5c73dvs1hnfv46b.apps.googleusercontent.com'
 
@@ -30,18 +45,39 @@ const Login = () => {
   }
   const formSubmitHandler = (e) => {
     e.preventDefault()
-    const data = {
-      age: age,
-      gender: gender,
-      state: state,
+    if (age < 18) {
+      setFormErrors({
+        ageErrorState: true,
+        ageError: 'Sorry - only candidates above 18 years of age are allowed',
+      })
+    } else {
+      setFormErrors({
+        ageErrorState: false,
+        ageError: '',
+      })
+      const data = {
+        age: age,
+        gender: gender,
+        state: state,
+      }
+      axios
+        .post(`https://sheetdb.io/api/v1/jcmfvgeqsyqk3`, data)
+        .then((res) => {
+          console.log(res)
+          setAge(0)
+          setGender('')
+          setState('')
+        })
     }
-
-    axios.post(`https://sheetdb.io/api/v1/jcmfvgeqsyqk3`, data).then((res) => {
-      console.log(res)
-      setAge(0)
-      setGender('')
-      setState('')
-    })
+  }
+  //   const selectStates=(e)=>{
+  //       statesList.filter(return statesList.indexOf()>-1)
+  //     setState(e.target.value)
+  //   }
+  const changeState = (val) => {
+    setState(val)
+    const dropdown = document.getElementsByClassName('dropdown')
+    console.log(dropdown)
   }
   return (
     <div>
@@ -66,15 +102,16 @@ const Login = () => {
                 value={age}
                 onChange={(e) => setAge(e.target.value)}
               />
-              {/* <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text> */}
+              {formErrors.ageErrorState && (
+                <Form.Text style={{ color: 'red' }}>
+                  {formErrors.ageError}
+                </Form.Text>
+              )}
             </Form.Group>
 
             <Form.Group className='mb-3' controlId='Gender'>
               <Form.Label>Gender</Form.Label>
               <Form.Select
-                placeholder='Password'
                 value={gender}
                 onChange={(e) => setGender(e.target.value)}
               >
@@ -83,15 +120,39 @@ const Login = () => {
                 <option value='female'>Female</option>
               </Form.Select>
             </Form.Group>
-            <Form.Group className='mb-3' controlId='State'>
-              <Form.Label>State of residence</Form.Label>
+
+            <div>
               <Form.Control
                 type='text'
-                placeholder='Normal text'
+                placeholder='State of Residence'
                 value={state}
                 onChange={(e) => setState(e.target.value)}
               />
-            </Form.Group>
+              {statesList
+                .filter((val) => {
+                  if (state == '') return val
+                  else if (val.toLowerCase().includes(state.toLowerCase()))
+                    return val
+                })
+                .map((val, key) => {
+                  return (
+                    <div className='dropdown' key={key}>
+                      <p
+                        className='dropdownItem'
+                        onClick={() => {
+                          setState(val)
+                          const dropdown =
+                            document.getElementsByClassName('dropdown')
+                          console.log(dropdown)
+                          dropdown[0].style.display = 'none'
+                        }}
+                      >
+                        {val}
+                      </p>
+                    </div>
+                  )
+                })}
+            </div>
 
             <Button variant='primary' type='submit'>
               Submit
